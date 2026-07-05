b1f67321da1a385527aa74f82ecde4cca7d1e56c
Kaylee <wky09060>
2026-07-05 10:10:00 +0800

    Initial Commit

---
+++ PayFlash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PayFlash
+//
+//  Created by Kaylee on 2026/7/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PayFlash/PayFlashApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayFlashApp.swift
+//  PayFlash
+//
+//  Created by Kaylee on 2026/7/5.
+//
+
+import SwiftUI
+
+@main
+struct PayFlashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
